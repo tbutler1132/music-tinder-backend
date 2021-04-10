2d0564736d3c25e163b7d4c08a8552a17e8e23d1
github username <tbutler1132@gmail.com>
2021-04-10 13:10:44 -0400

    Friend Request

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist_name, :role, :genre, :location, :tags, :email, :bio, :matches
+  attributes :id, :name, :artist_name, :role, :genre, :location, :tags, :email, :bio
 
   has_many :demos
   has_many :likers
   has_many :liked
   
-  has_many :first
-  has_many :second
+  # has_many :first
+  # has_many :second
 end
